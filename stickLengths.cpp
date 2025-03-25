1b3d1477618e5a3b9a4db77fc99f85c4c0aa050f
Sriram Bharadwaj <sriram9217@gmail.com>
2025-03-25 21:39:43 +0530

    solved stick lengths

---
+++ stickLengths.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// expected tc :- nlogn
+// expected sc :- n
+
+
+int main(){
+    int n;
+    std::cin >> n;
+    std::vector<int> a(n);
+
+    for(auto &x : a) std::cin >> x;
+    std::sort(a.begin(), a.end());
+
+    int median = a[n/2];
+    
+    long long res = 0;
+    
+    for(int i = 0;i<n;i++) res += abs(a[i] - median);
+
+    std::cout << res;
+    
+
+    return 0;
+}
